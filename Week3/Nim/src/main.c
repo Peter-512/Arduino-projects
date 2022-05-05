25237c71975818ee1dc2daa3b9cec67016026d17
Peter <peter.buschenreiter@student.kdg.be>
2022-05-05 16:07:11 +0200

    Game completly functioning

--- Week3/Nim/src/main.c
+++ Week3/Nim/src/main.c
@@ -11,17 +11,25 @@
 #define true 1
 #define false !true
 
-#define DEBUG
+// #define DEBUG
 #ifdef DEBUG
 #include <usart.h>
 #endif
 
-uint8_t button; // ? Maybe this could be a local variable of the ISR
-uint8_t btnPushed = false;
-uint8_t firstButtonPush = true;
+uint8_t firstButtonPush = false;
 uint8_t matchesToTake = 1;
 char players[] = "PC";
 char *currentPlayer = players;
+uint8_t matches = START_NUMBER;
+
+void swapPlayers()
+{
+	currentPlayer++;
+	if (!*currentPlayer)
+	{
+		currentPlayer -= 2;
+	}
+}
 
 /**
  * @brief Construct a new ISR object
@@ -30,33 +38,40 @@ char *currentPlayer = players;
  */
 ISR(PCINT1_vect)
 {
-	button = whichButtonPushed();
-	if (!firstButtonPush)
+	uint8_t button = whichButtonPushed();
+	if (firstButtonPush)
 	{
 		switch (button)
 		{
 		case BUTTON1:
-			matchesToTake = (matchesToTake + 1) % MAX_NUMBER + 1;
+			matchesToTake--; // ? this is an ugly solution but im too tired
+			if (matchesToTake == 0)
+			{
+				matchesToTake += MAX_NUMBER;
+			}
 			break;
 		case BUTTON2:
-			currentPlayer++;
-			if (!*currentPlayer)
+			swapPlayers();
+			matches -= matchesToTake;
+			if (*currentPlayer == players[1]) // * if currentPlayer is C
 			{
-				currentPlayer -= 2;
+				matchesToTake = (matches - 1) % (MAX_NUMBER + 1);
+			}
+			else
+			{
+				matchesToTake = 1;
 			}
 			break;
 		case BUTTON3:
-			matchesToTake = (matchesToTake - 1 + MAX_NUMBER) % MAX_NUMBER + 1;
+			matchesToTake = matchesToTake % MAX_NUMBER + 1;
 			break;
-
 		default:
 			break;
 		}
 	}
 	if (button < NUMBER_OF_BUTTONS)
 	{
-		btnPushed = true;
-		firstButtonPush = false;
+		firstButtonPush = true;
 	}
 }
 
@@ -65,7 +80,7 @@ void setup()
 	enableAllButtons();
 	getButtonsReadyForInterrupts();
 	enableAllLeds();
-	while (!btnPushed)
+	while (!firstButtonPush)
 	{
 		for (int j = 0; j < 4; j++)
 		{
@@ -78,23 +93,31 @@ void setup()
 	initADC();
 	uint16_t seed = getPotentiometerValue();
 	srand(seed);
-	writeNumberAndWait(seed, 5000);
-	btnPushed = false;
+	writeNumberAndWait(seed, 2000);
 }
 
 void displayScreen()
 {
+	writeNumberToSegment(0, matchesToTake);
 	writeLetterToSegment(1, *currentPlayer);
+	writeNumberToSegment(2, (matches / 10) % 10);
+	writeNumberToSegment(3, matches % 10);
 }
 
 int main(int argc, char const *argv[])
 {
-	uint8_t currentMatchAmount = START_NUMBER;
 	setup();
 
-	while (currentMatchAmount > 0)
+	while (matches > 0)
 	{
 		displayScreen();
+	}
+	while (1)
+	{
+		writeLetterToSegment(0, *currentPlayer);
+		writeLetterToSegment(1, 'W');
+		writeLetterToSegment(2, 'O');
+		writeLetterToSegment(3, 'N');
 	}
 	return 0;
 }
