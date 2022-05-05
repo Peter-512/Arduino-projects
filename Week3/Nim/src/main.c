684cdc1010c9dd2c85026c8649f9fcde6bc67c64
Peter <peter.buschenreiter@student.kdg.be>
2022-05-05 08:41:52 +0200

    Setup method, init. of things, setting seed

--- Week3/Nim/src/main.c
+++ Week3/Nim/src/main.c
@@ -1,4 +1,28 @@
+#include <avr/io.h>
+#include <button-lib.h>
+#include <display.h>
+#include <stdlib.h>
+#include <potentiometer.h>
+#define START_NUMBER 21
+#define MAX_NUMBER 3
+
+#define DEBUG
+#ifdef DEBUG
+#include <usart.h>
+#endif
+
+void setup()
+{
+	initDisplay();
+	initADC();
+	uint16_t seed = getPotentiometerValue();
+	srand(seed);
+	writeNumberAndWait(seed, 10000);
+}
+
 int main(int argc, char const *argv[])
 {
-  return 0;
+	uint8_t currentMatchAmount = START_NUMBER;
+	setup();
+	return 0;
 }
